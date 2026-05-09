7ed531edc73395d05737abe82268c26a1eb1e139
Sandhyarani29 <sandhyaranimaske58@gmail.com>
2026-05-09 17:18:16 +0530

    Fix: correct API base URL ending with /api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://hospitalmanagementapplication20260418005920-dzfyechah9b2dzg7.canadacentral-01.azurewebsites.net/swagger/index.html',
+  baseURL: 'https://hospitalmanagementapplication20260418005920-dzfyechah9b2dzg7.canadacentral-01.azurewebsites.net/api',
 });
 
 export default API;
