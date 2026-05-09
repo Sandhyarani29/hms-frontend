454a74d1d747abd08d18777dab2de95d9d015305
Sandhyarani29 <sandhyaranimaske58@gmail.com>
2026-05-09 11:36:10 +0530

    Fix: correct Azure API base URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
-
 import axios from 'axios';
-const API=axios.create({
-  baseURL: 'https://https://hospitalmanagementapplication20260418005920-dzfyechah9b2dzg7.canadacentral-01.azurewebsites.net/swagger/index.htmlhospitalmanagementapplication20260418005920-dzfyechah9b2dzg7.canadacentral-01.azurewebsites.net/api'
-})
+
+const API = axios.create({
+  baseURL: 'https://hospitalmanagementapplication20260418005920-dzfyechah9b2dzg7.canadacentral-01.azurewebsites.net/swagger/index.html',
+});
 
 export default API;
